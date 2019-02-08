350532c19bcbea3f0201e5f5ad7e06bd1d33654e
noamteyssier <nb.teyssier@gmail.com>
2019-02-08 12:07:04 -0800

    added ggplot coloring and options for oossp visualizations

--- src/haplotypeDistances.py
+++ src/haplotypeDistances.py
@@ -7,24 +7,27 @@ from ggplot import *
 import sys
 
 class HaplotypeUtils:
-    def __init__(self, dist, vcf, sdo):
+    def __init__(self, dist, vcf, sdo, meta):
         self.dist_fn = dist
         self.vcf_fn = vcf
         self.sdo_fn = sdo
+        self.meta_fn = meta
 
         self.dist = None # pd.DataFrame()
         self.vcf = None # pd.DataFrame()
         self.sdo = None # pd.DataFrame()
+        self.meta = None # pd.DataFrame()
 
         self.melted_dist = None # pd.DataFrame()
         self.one_off = None # pd.DataFrame()
         self.same_sample_pairs = None # pd.DataFrame()
         self.oossp = None # pd.DataFrame()
+        self.melted_oossp = None # pd.DataFrame()
 
         self.__load_dist__()
         self.__load_vcf__()
         self.__load_sdo__()
-
+        self.__load_meta__()
     def __load_dist__(self):
         """read in filename for snp distances into dataframe"""
         self.dist = pd.read_csv(self.dist_fn, sep="\t").\
@@ -36,6 +39,30 @@ class HaplotypeUtils:
     def __load_sdo__(self):
         """read in filename seekdeep output into dataframe"""
         self.sdo = pd.read_csv(self.sdo_fn, sep="\t")
+        self.sdo[['date', 'cohortid']] = self.sdo.apply(
+            lambda x : self.__split_cid_date__(x), axis = 1, result_type = 'expand')
+    def __load_meta__(self):
+        """parses statabase13 for relevant columns"""
+        self.meta = pd.read_stata(self.meta_fn)[['cohortid', 'date', 'qpcr']]
+
+        # convert cohortid to string for joining
+        self.meta.cohortid = self.meta.apply(
+            lambda x : str(x.cohortid), axis = 1)
+
+        # apply filters for only cid in sampleset and routine visits except for malaria events
+        self.meta = self.meta[self.meta.cohortid.isin(self.sdo.cohortid)]
+
+        self.meta.date = self.meta.\
+            apply(lambda x : x.date.strftime('%Y-%m-%d'), axis = 1)
+
+        # create s_Sample column from date and cohortid
+        self.meta['s_Sample'] = self.meta.\
+            apply(lambda x : '-'.join([x.date, str(x.cohortid)]), axis=1)
+    def __split_cid_date__(self, row):
+        """convert s_Sample to date and cohortid"""
+        a = row.s_Sample.split('-')
+        date, cid = '-'.join(a[:3]), a[-1]
+        return [date, cid]
     def __NaN_upper_triangular__(self, df, colskip=1):
         """
         - convert upper triangular of dataframe to NaN
@@ -165,6 +192,55 @@ class HaplotypeUtils:
             [df.h_popUID1, df.h_popUID2, df.h1_fraction, df.h2_fraction])
         df.drop(columns = ['uid1', 'uid2'], inplace=True)
         return df
+    def __haplotype_occurence__(self):
+        """create dataframe of haplotype occurences in dataset"""
+        self.haplotype_occurences = self.sdo.h_popUID.\
+            value_counts()
+    def __flag_hvlines__(self, row, vlines, hlines):
+        """assign flags to vline and hlines to categorize dataframe for plotting"""
+        return_array = []
+        for i in vlines:
+            if row.h2_fraction > max(vlines):
+                return_array.append(">%.2f" % max(vlines))
+                break
+            if row.h2_fraction < i:
+                return_array.append('<%.2f' %i)
+                break
+
+        for j in hlines:
+            if row.majorRatio > max(hlines):
+                return_array.append('>%i' % max(hlines))
+                break
+            if row.majorRatio < j:
+                return_array.append('<%i' %j)
+                break
+        return return_array
+    def __flag_occurence__(self, row):
+        """flag occurence of haplotype"""
+        oc = self.haplotype_occurences[row.h_popUID2]
+        if oc == 1 :
+            return '1'
+        elif oc == 2:
+            return '2'
+        elif oc == 3:
+            return '3'
+        elif oc < 10:
+            return '<10'
+        else:
+            return '>10'
+    def __flag_qpcr__(self, qpcr):
+        """bin qpcr into log10 densities"""
+        if qpcr == 0:
+            return '0'
+        if int(np.log10(qpcr)) == 1:
+            return '1'
+        elif (int(np.log10(qpcr))) == 2:
+            return '2'
+        elif (int(np.log10(qpcr))) == 3:
+            return '3'
+        elif (int(np.log10(qpcr))) == 4:
+            return '4'
+        return '5'
     def FindOneOff(self):
         """return dataframe of all haplotypes one snp off from each other"""
         self.__melted_dist__()
@@ -182,26 +258,60 @@ class HaplotypeUtils:
             return self.__melted_oossp__()
         else:
             return self.oossp
+    def PlotOOSSP(self, vlines, hlines, color_type = 'fraction'):
+        self.__oossp__()
+        if color_type == 'fraction':
+            self.oossp[['c_Fraction', 'c_Ratio']] = self.oossp.\
+                apply(lambda x : self.__flag_hvlines__(x, vlines, hlines),
+                result_type = 'expand', axis = 1)
+
+            g = ggplot(self.oossp, aes(x = 'h2_fraction', y = 'majorRatio', color = 'c_Fraction')) +\
+                geom_point() +\
+                geom_vline(x = vlines, linetype = 'dashed', color = 'grey') +\
+                geom_hline(y = hlines, linetype = 'dashed', color = 'grey') +\
+                theme_bw()
+            print(g)
+        elif color_type == 'occurence':
+            self.__haplotype_occurence__()
+            self.oossp['h2_occurence'] = self.oossp.apply(
+                lambda x : self.__flag_occurence__(x), axis = 1)
+
+            g = ggplot(self.oossp, aes(x = 'h2_fraction', y = 'majorRatio', color = 'h2_occurence')) +\
+                geom_point(size = 40, alpha = 0.8) +\
+                geom_vline(x = vlines, linetype = 'dashed', color = 'grey') +\
+                geom_hline(y = hlines, linetype = 'dashed', color = 'grey') +\
+                theme_bw() +\
+                scale_color_manual(values = ['peru', 'red', 'blue', 'green', 'black'])
+            print(g)
+        elif color_type == 'density':
+            self.oossp = self.oossp.merge(self.meta[['s_Sample', 'qpcr']])
+            self.oossp = self.oossp[self.oossp.qpcr > 0]
+            self.oossp['log10_qpcr'] = self.oossp.apply(
+                lambda x : self.__flag_qpcr__(x.qpcr), axis = 1)
+
+            g = ggplot(self.oossp, aes(x = 'h2_fraction', y = 'majorRatio', color = 'log10_qpcr')) +\
+                geom_point(size = 50, alpha = 0.5) +\
+                geom_vline(x = vlines, linetype = 'dashed', color = 'grey') +\
+                geom_hline(y = hlines, linetype = 'dashed', color = 'grey') +\
+                theme_bw() +\
+                scale_color_manual(values = ['peru', 'red', 'blue', 'green', 'black'])
+            print(g)
+
 
 def main():
     fasta = '/home/noam/bin/ama1/prism2/full_prism2/pfama1.fasta'
     dist = '/home/noam/bin/ama1/prism2/full_prism2/pfama1.dist'
     vcf = '/home/noam/bin/ama1/prism2/full_prism2/pfama1.vcf'
     sdo = '/home/noam/bin/ama1/prism2/full_prism2/pfama1_sampInfo.tab.txt'
+    meta = '/home/noam/bin/ama1/prism2/stata/allVisits.dta'
 
-    h = HaplotypeUtils(dist, vcf, sdo)
-    oossp = h.FindOneOffSameSamplePairs()
-    melted_oossp = h.FindOneOffSameSamplePairs(melted=True)
+    h = HaplotypeUtils(dist, vcf, sdo, meta)
+    h.PlotOOSSP(
+        vlines = [0.01, 0.02, 0.03],
+        hlines = [50, 100],
+        color_type = 'fraction'
+        )
 
-    g = ggplot(melted_oossp, aes(x = 'fraction', fill = 'hap_class')) +\
-        geom_histogram(position = 'identity', alpha = 0.5, bins = 100) +\
-        scale_fill_manual(values = ['peru', 'maroon'])
-    print(g)
-
-    g = ggplot(oossp, aes(x = 'h2_fraction', y = 'majorRatio')) +\
-        geom_point() +\
-        geom_vline(x = [0.01, 0.02, 0.03])
-    print(g)
 
 
 if __name__ == '__main__':
