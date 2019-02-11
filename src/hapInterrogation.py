be229a3b360fdaf4c37b6cfbb836e1a5e0904baf
noamteyssier <nb.teyssier@gmail.com>
2019-02-11 14:59:22 -0800

    interrogative function examples

---
+++ src/hapInterrogation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+import pandas as pd
+import numpy as np
+from seekdeep_modules import SeekDeepUtils
+
+def main():
+    sdo_fn = "../prism2/full_prism2/pfama1_sampInfo.tab.txt"
+    meta_fn = "../prism2/stata/allVisits.dta"
+    sdo = pd.read_csv(sdo_fn, sep = "\t")
+    meta = pd.read_stata(meta_fn)
+
+    s = SeekDeepUtils()
+    a_frequency = s.Time_Independent_Allele_Frequency(sdo)
+    hapSkips = s.Haplotype_Skips(sdo, meta)
+
+if __name__ == '__main__':
+    main()
