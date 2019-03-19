0ee4fd34e93a83bc475e97eb17d397257658c93a
noamteyssier <nb.teyssier@gmail.com>
2019-03-19 16:50:06 -0700

    added rolling *rators and plotting distributions of HH

--- src/householdClustering.py
+++ src/householdClustering.py
@@ -7,9 +7,12 @@ import matplotlib.pyplot as plt
 
 from itertools import combinations
 from scipy.spatial.distance import squareform, pdist
+from numpy.random import shuffle
 
 import sys
 
+
+sns.set(rc={'figure.figsize':(15, 12), 'lines.linewidth': 5})
 class SpatialClustering:
     def __init__(self, sdo, meta):
         self.sdo_fn = sdo
@@ -56,7 +59,7 @@ class SpatialClustering:
         self.cHap = self.data[['h_popUID', 'cohortid']].drop_duplicates()
         self.cHap['z'] = 1
         self.cHap = self.meta.merge(self.cHap, how='left')
-    def frequency_of_identity(self, x):
+    def frequency_of_identity(self, x, rolling = False):
         """Number of matching haplotypes in set divided by number of comparisons"""
         if x.shape[0] > 1:
             pv = x[['h_popUID', 'cohortid', 'z']].\
@@ -67,7 +70,7 @@ class SpatialClustering:
                 ).fillna(0)
 
             pv = pv.loc[:, pv.columns.notnull()]
-            if not pv.empty:
+            if not pv.empty and pv.shape[0] > 1:
 
                 # sum of (n * (n-1)) / 2 for each haplotype
                 colsum = pv.sum(axis=0)
@@ -78,11 +81,13 @@ class SpatialClustering:
                 matsum = np.where(pv > 0)[0].size
                 denominator = (rowsum * (matsum - rowsum)).sum()
 
+                if rolling:
+                    return [numerator, denominator]
+
                 return numerator / denominator
 
         return 0
-
-    def clusterHH(self, infOnly=True):
+    def clusterHH(self, infOnly=True, shuffle_hhid=False, population=True, rolling=False):
         """
         Pr(Zi == Zj | j in householdSet) /
         Pr(Zi == Zj | j in populationSet)
@@ -91,28 +96,65 @@ class SpatialClustering:
         if infOnly :
             self.cHap = self.cHap[self.cHap.z>0]
 
+        if shuffle_hhid :
+            shuffle(self.cHap.hhid.values)
+
+
         # numerator
-        a = self.cHap.groupby('hhid').apply(lambda x : self.frequency_of_identity(x))
+        a = self.cHap.groupby('hhid').apply(lambda x : self.frequency_of_identity(x, rolling=rolling))
+
+        if rolling:
+            nums = []
+            dems = []
+            for i in a:
+                if type(i) == list:
+                    nums.append(i[0])
+                    dems.append(i[1])
+
+            nums = np.array(nums)
+            dems = np.array(dems)
+            a = nums.sum() / dems.sum()
 
         # denominator
-        b = self.frequency_of_identity(self.cHap)
+        if population:
+            b = self.frequency_of_identity(self.cHap)
+            print(
+                "household similarity : {0}\npopulation similarity : {1}\nratio : {2}".\
+                format(a.mean(), b, a.mean()/b))
+        else:
+            b = 1
 
-        print(
-            "household similarity : {0}\npopulation similarity : {1}\nratio : {2}".\
-            format(a.mean(), b, a.mean()/b))
 
-        return (a, b, a.mean()/b)
-
-
+        # sys.exit(a)
+        return [a, b]
 def main():
     sdo = '../prism2/full_prism2/filtered_5pc_10r.tab'
     meta = '../prism2/stata/allVisits.dta'
     sc = SpatialClustering(sdo, meta)
-    a, b, amb = sc.clusterHH()
+
+    # remove outlier
+    sc.cHap = sc.cHap[sc.cHap.hhid != 131011801]
+
+    # calculate mean with rolling *erators
+    a, b = sc.clusterHH(rolling=True)
+    a.mean()/b
+
+    #sns.distplot(a, bins=30, kde=False)
+
+    # run permutations test
+    permutations = np.array([sc.clusterHH(population=False)[0]] + [sc.clusterHH(shuffle_hhid=True, population=False)[0] for _ in range(500)])
+
+    # calculate upper and lower bounds of permutation means
+    lower, higher = np.quantile(permutations[1:].mean(axis=1), [0.025, 1 - 0.025]) / b
+    means = permutations[1:].mean(axis=1)
+    means[means > higher].size
+
+    # plot distribution of permutations and observed calculation
+    sns.distplot(means/b, bins = 100)
+    plt.axvline(a.mean()/b)
 
 
 
-    pass
 
 if __name__ == '__main__':
     main()
