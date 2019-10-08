fa59d8b54b3384090d06c06df405ec54eb2da192
noamteyssier <nb.teyssier@gmail.com>
2019-10-08 14:01:29 -0700

    hapinterrogation implementing pkgpr2 module

--- src/hapInterrogation.py
+++ src/hapInterrogation.py
@@ -3,67 +3,107 @@
 
 import pandas as pd
 import numpy as np
-import argparse, sys
+import argparse
+import sys
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-from ama1 import InfectionLabeler, FOI, ExponentialDecay, Survival, FractionOldNew, OldNewSurival, OldWaning
+from pkgpr2.pkgpr2 import InfectionLabeler, FOI, ExponentialDecay
+from pkgpr2.pkgpr2 import OldWaning, FractionOldNew, OldNewSurival
+
 
 def get_args():
     p = argparse.ArgumentParser()
-    p.add_argument('-i', '--seekdeep_output', required=False,
+    p.add_argument(
+        '-i', '--seekdeep_output', required=False,
         default="../prism2/full_prism2/final_filter.tab",
-        help="SeekDeep Output to use as input to functions")
-    p.add_argument('-m', '--meta', required=False,
-        default= "../prism2/stata/full_meta_6mo_fu.tab",
+        help="SeekDeep Output to use as input to functions"
+        )
+    p.add_argument(
+        '-m', '--meta', required=False,
+        default="../prism2/stata/full_meta_6mo_fu.tab",
         help="Cohort Meta information (tsv) to relate cohortids")
-    p.add_argument('-l', '--label_infections',
+    p.add_argument(
+        '-l', '--label_infections',
         action='store_true',
-        help="Create a dataframe showing the start and end of infection of each cohortid~h_popUID"
+        help=(
+            "Create a dataframe showing the start and end of"
+            "infection of each cohortid~h_popUID"
+            )
         )
-    p.add_argument('-f', '--force_of_infection',
+    p.add_argument(
+        '-f', '--force_of_infection',
         type=str,
-        help="Calculate the Force of Infection of the Population (give grouping variables comma separated or write 'none' for no grouping)")
-    p.add_argument('-d', '--durations',
+        help=(
+            "Calculate the Force of Infection of the Population"
+            "(give grouping variables comma separated "
+            "or write 'none' for no grouping)"
+            )
+        )
+    p.add_argument(
+        '-d', '--durations',
         type=str,
-        help="Estimate duration of infections using an exponential decay model (give grouping variables comma separated or write 'none' for no grouping)"
+        help=(
+            "Estimate duration of infections using an exponential decay model "
+            "(give grouping variables comma separated"
+            "or write 'none' for no grouping)")
         )
-    p.add_argument('-s', '--survival',
+    p.add_argument(
+        '-s', '--survival',
         type=str,
-        help='Plot survival of old/new haplotypes (options : fraction_oldnew, survival_oldnewmix, waning_old)'
+        help=(
+            'Plot survival of old/new haplotypes '
+            '(options : fraction_oldnew, survival_oldnewmix, waning_old)'
+            )
         )
-    p.add_argument('-n', '--skip_threshold',
-        default = 3,
-        type=int,
-        help="Number of allowed skips to allow during calculation of durations (default = 3 skips)"
-        )
-    p.add_argument('-b', '--burnin',
+    p.add_argument(
+        '-n', '--skip_threshold',
         default=3,
         type=int,
-        help="Number of months to consider a patient in burnin period (default = 3 months)"
+        help=(
+            "Number of allowed skips to allow during calculation of durations "
+            "(default = 3 skips)"
+            )
         )
-    p.add_argument('-q', '--qpcr_threshold',
+    p.add_argument(
+        '-b', '--burnin',
+        default=3,
+        type=int,
+        help=(
+            "Number of months to consider a patient in burnin period "
+            "(default = 3 months)"
+            )
+        )
+    p.add_argument(
+        '-q', '--qpcr_threshold',
         default=0,
         type=float,
         required=False,
         help="qpcr threshold to consider a sample (set to 0 for no threshold)"
         )
-    p.add_argument('--by_infection_event',
+    p.add_argument(
+        '--by_infection_event',
         action='store_true',
         required=False,
         help='Collapse infection events by individual'
         )
-    p.add_argument('--no_impute',
+    p.add_argument(
+        '--no_impute',
         action='store_false',
         required=False,
         help='Dont impute missing haplotypes when collapsing by date '
         )
-    p.add_argument('--no_aggregation',
+    p.add_argument(
+        '--no_aggregation',
         action='store_false',
         required=False,
-        help='Dont aggregate infection events following the skip rule when collapsing by date'
+        help=(
+            'Dont aggregate infection events following the skip '
+            'rule when collapsing by date'
+            )
         )
-    p.add_argument("--num_bootstraps",
+    p.add_argument(
+        "--num_bootstraps",
         default=200,
         type=int,
         help='number of times to bootstrap'
@@ -76,23 +116,28 @@ def get_args():
 
     args = p.parse_args()
     return args
+
+
 def print_out(df):
     """simple printout for a pandas dataframe to stdout"""
     df.to_csv(sys.stdout, sep="\t", index=False)
 
+
 def label_infections(sdo, meta, args):
     il = InfectionLabeler(
         sdo, meta,
-        qpcr_threshold = args.qpcr_threshold,
-        burnin = args.burnin,
-        skip_threshold = args.skip_threshold,
-        by_infection_event = args.by_infection_event,
-        impute_missing = args.no_impute,
-        agg_infection_event = args.no_aggregation
+        qpcr_threshold=args.qpcr_threshold,
+        burnin=args.burnin,
+        skip_threshold=args.skip_threshold,
+        by_infection_event=args.by_infection_event,
+        impute_missing=args.no_impute,
+        agg_infection_event=args.no_aggregation
     )
 
     labels = il.LabelInfections()
     labels.to_csv(sys.stdout, sep="\t", index=False)
+
+
 def foi(sdo, meta, args):
     group = args.force_of_infection.split(',')
 
@@ -115,20 +160,21 @@ def foi(sdo, meta, args):
 
     il = InfectionLabeler(
         sdo, meta,
-        qpcr_threshold = args.qpcr_threshold,
-        burnin = args.burnin,
-        skip_threshold = args.skip_threshold,
-        by_infection_event = args.by_infection_event,
-        impute_missing = args.no_impute,
-        agg_infection_event = args.no_aggregation
+        qpcr_threshold=args.qpcr_threshold,
+        burnin=args.burnin,
+        skip_threshold=args.skip_threshold,
+        by_infection_event=args.by_infection_event,
+        impute_missing=args.no_impute,
+        agg_infection_event=args.no_aggregation
     )
 
     labels = il.LabelInfections()
-    f = FOI(labels, meta, burnin = args.burnin)
-    result = f.fit(group = group)
+    f = FOI(labels, meta, burnin=args.burnin)
+    result = f.fit(group=group)
     result.to_csv(sys.stdout, sep="\t", index=True)
 
-def DecayByGroup(infections, n_iter=100, group = ['gender'], label=None):
+
+def DecayByGroup(infections, n_iter=100, group=['gender'], label=None):
     ed_classes = []
     estimated_values = []
     bootstrapped_values = []
@@ -143,13 +189,19 @@ def DecayByGroup(infections, n_iter=100, group = ['gender'], label=None):
         bootstrapped_values.append(bsl)
 
     for i, _ in enumerate(ed_classes):
-        sns.distplot(1 /bootstrapped_values[i], bins=30)
-        plt.axvline(1/ estimated_values[i], label=indices[i], color=sns.color_palette()[i], linestyle=':', lw=5)
-        plt.legend(labels = indices)
+        sns.distplot(1 / bootstrapped_values[i], bins=30)
+        plt.axvline(
+            1 / estimated_values[i], label=indices[i],
+            color=sns.color_palette()[i],
+            linestyle=':', lw=5
+            )
+        plt.legend(labels=indices)
 
     if label:
         plt.savefig('../plots/durations/{}.png'.format(label))
     plt.show()
+
+
 def durations(sdo, meta, args):
     group = args.durations.split(',')
 
@@ -169,21 +221,23 @@ def durations(sdo, meta, args):
             sys.exit()
     il = InfectionLabeler(
         sdo, meta,
-        qpcr_threshold = args.qpcr_threshold,
-        burnin = args.burnin,
-        skip_threshold = args.skip_threshold,
-        by_infection_event = args.by_infection_event,
-        impute_missing = args.no_impute,
-        agg_infection_event = args.no_aggregation
+        qpcr_threshold=args.qpcr_threshold,
+        burnin=args.burnin,
+        skip_threshold=args.skip_threshold,
+        by_infection_event=args.by_infection_event,
+        impute_missing=args.no_impute,
+        agg_infection_event=args.no_aggregation
     )
 
     labels = il.LabelInfections()
     if group:
-        DecayByGroup(labels, n_iter=args.num_bootstraps, group = group)
+        DecayByGroup(labels, n_iter=args.num_bootstraps, group=group)
     else:
         e = ExponentialDecay(labels)
         e.fit(bootstrap=True, n_iter=args.num_bootstraps)
         e.plot()
+
+
 def survival(sdo, meta, args):
 
     known_methods = ['fraction_oldnew', 'survival_oldnewmix', 'waning_old']
@@ -192,33 +246,43 @@ def survival(sdo, meta, args):
 
     il = InfectionLabeler(
         sdo, meta,
-        qpcr_threshold = args.qpcr_threshold,
-        burnin = args.burnin,
-        skip_threshold = args.skip_threshold,
-        by_infection_event = args.by_infection_event,
-        impute_missing = args.no_impute,
-        agg_infection_event = args.no_aggregation
+        qpcr_threshold=args.qpcr_threshold,
+        burnin=args.burnin,
+        skip_threshold=args.skip_threshold,
+        by_infection_event=args.by_infection_event,
+        impute_missing=args.no_impute,
+        agg_infection_event=args.no_aggregation
     )
 
     labels = il.LabelInfections()
 
-    if known_methods.index(args.survival) == 0 :
-        fon = FractionOldNew(infections=labels, meta=meta, burnin=args.burnin, bootstrap=True, n_iter=args.num_bootstraps)
+    if known_methods.index(args.survival) == 0:
+        fon = FractionOldNew(
+            infections=labels, meta=meta, burnin=args.burnin,
+            bootstrap=True, n_iter=args.num_bootstraps
+            )
         fon.fit()
         fon.plot()
 
-    elif known_methods.index(args.survival) == 1 :
-        ons = OldNewSurival(infections=labels, meta=meta, burnin=args.burnin, bootstrap=True, n_iter=args.num_bootstraps)
+    elif known_methods.index(args.survival) == 1:
+        ons = OldNewSurival(
+            infections=labels, meta=meta, burnin=args.burnin,
+            bootstrap=True, n_iter=args.num_bootstraps
+            )
         ons.fit()
         ons.plot()
     else:
-        w = OldWaning(infections = labels, meta=meta, burnin=args.burnin, bootstrap=True, n_iter=args.num_bootstraps)
+        w = OldWaning(
+            infections=labels, meta=meta, burnin=args.burnin,
+            bootstrap=True, n_iter=args.num_bootstraps
+            )
         w.fit()
         w.plot()
 
+
 def main():
     args = get_args()
-    sdo = pd.read_csv(args.seekdeep_output, sep = "\t")
+    sdo = pd.read_csv(args.seekdeep_output, sep="\t")
     meta = pd.read_csv(args.meta, sep="\t", low_memory=False)
 
     if args.label_infections:
@@ -229,6 +293,7 @@ def main():
         durations(sdo, meta, args)
     elif args.survival:
         survival(sdo, meta, args)
+
 
 if __name__ == '__main__':
     main()
